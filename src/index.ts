c507476175247877b701312de712c119a2c392ec
liting <luz.liting@gmail.com>
2023-06-24 18:59:43 +0800

    feat: export MonacoEditor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Repl } from './Repl.vue'
 export { default as Preview } from './output/Preview.vue'
+export { default as MonacoEditor } from './editor/MonacoEditor.vue'
 export { ReplStore, File } from './store'
 export { compileFile } from './transform'
 export type { Props as ReplProps } from './Repl.vue'
