9086876ab333eed46ab58a5509f6c932f75c2e5f
liting <luz.liting@gmail.com>
2023-06-24 20:13:45 +0800

    fix(type): export error about UpdateFlag

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 export { default as Repl } from './Repl.vue'
-export {
-  default as Preview,
-  UpdateFlag as PreviewUpdateFlag,
-} from './output/Preview.vue'
+export { default as Preview } from './output/Preview.vue'
 export { default as MonacoEditor } from './editor/MonacoEditor.vue'
 export { ReplStore, File, defaultMainFile } from './store'
 export { compileFile } from './transform'
 export type { Props as ReplProps } from './Repl.vue'
 export type { Store, StoreOptions, SFCOptions, StoreState } from './store'
 export type { OutputModes } from './output/types'
+export type { UpdateFlag as PreviewUpdateFlag } from './output/Preview.vue'
