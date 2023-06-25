4f4ac3449f845240e992d63a827305465f236dc5
liting <luz.liting@gmail.com>
2023-06-25 08:24:46 +0800

    test: add VuePreview for test

---
+++ test/VuePreview.vue
@@ -0,0 +1,269 @@
+<script setup lang="ts">
+import type { PreviewUpdateFlag, Store } from '../src'
+import { MonacoEditor, Preview, ReplStore, defaultMainFile } from '../src'
+import { computed, onMounted, provide, ref } from 'vue'
+import { useClipboard, useDebounceFn, useElementHover } from '@vueuse/core'
+import Copy from './icons/Copy.vue'
+import Copied from './icons/Copied.vue'
+import UnfoldLess from './icons/UnfoldLess.vue'
+import UnfoldMore from './icons/UnfoldMore.vue'
+import Loading from './icons/Loading.vue'
+
+export interface Props {
+  store?: Store
+  autoResize?: boolean
+  clearConsole?: boolean
+  ssr?: boolean
+  code?: string
+  encode?: boolean
+  collapse?: boolean
+  previewBodyStyle?: Partial<CSSStyleDeclaration> | string
+  previewAppStyle?: Partial<CSSStyleDeclaration> | string
+  previewOptions?: {
+    headHTML?: string
+    bodyHTML?: string
+    customCode?: {
+      importCode?: string
+      useCode?: string
+    }
+  }
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  store: () => new ReplStore(),
+  autoResize: true,
+  clearConsole: true,
+  ssr: false,
+  encode: false,
+  collapse: false,
+  previewOptions: () => ({
+    headHTML: '',
+    bodyHTML: '',
+    customCode: {
+      importCode: '',
+      useCode: '',
+    },
+  }),
+})
+
+const { store } = props
+
+const welcomeCode = `
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('vite-plugin-vue-preview')
+<\/script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="msg">
+</template>
+`.trim()
+
+if (!props.code) {
+  store.setFiles({
+    [defaultMainFile]: welcomeCode,
+  })
+} else if (props.encode) {
+  store.setFiles({
+    [defaultMainFile]: decodeURIComponent(props.code),
+  })
+} else {
+  store.setFiles({
+    [defaultMainFile]: props.code,
+  })
+}
+
+onMounted(() => {
+  if (props.clearConsole)
+    // eslint-disable-next-line no-console
+    console.clear()
+})
+
+const previewUpdateFlag = ref<PreviewUpdateFlag>('UPDATING')
+function onPreivewUpdatePreview(flag: PreviewUpdateFlag) {
+  previewUpdateFlag.value = flag
+}
+
+store.init()
+
+const onChange = useDebounceFn((code: string) => {
+  store.state.activeFile.code = code
+}, 250)
+
+provide('store', store)
+provide('autoresize', props.autoResize)
+provide('clear-console', props.clearConsole)
+provide('preview-options', props.previewOptions)
+
+const { copy, copied } = useClipboard({
+  source: store.state.activeFile.code,
+  legacy: true,
+})
+
+const isCollapse = ref(props.collapse)
+const maxHeightForCode = computed(() => (isCollapse.value ? '0' : '1000px'))
+
+const previewBodyStyle = computed<Partial<CSSStyleDeclaration>>(() =>
+  typeof props.previewBodyStyle === 'string'
+    ? JSON.parse(decodeURIComponent(props.previewBodyStyle))
+    : props.previewBodyStyle
+)
+const previewAppStyle = computed<Partial<CSSStyleDeclaration>>(() =>
+  typeof props.previewAppStyle === 'string'
+    ? JSON.parse(decodeURIComponent(props.previewAppStyle))
+    : props.previewAppStyle
+)
+
+const vuePreviewContainerRef = ref()
+const isHover = useElementHover(vuePreviewContainerRef)
+</script>
+
+<template>
+  <div class="vue-preview">
+    <div ref="vuePreviewContainerRef" class="vue-preview__container">
+      <Preview
+        show
+        :ssr="props.ssr"
+        :body-style="previewBodyStyle"
+        :app-style="previewAppStyle"
+        @update-preview="onPreivewUpdatePreview"
+      />
+      <Transition
+        v-show="previewUpdateFlag !== 'UPDATING' && isHover"
+        name="vue-preview-slide-down"
+      >
+        <div class="vue-preview__btns">
+          <button
+            v-show="!copied"
+            title="copy code"
+            @click="copy(store.state.activeFile.code)"
+          >
+            <Copy />
+          </button>
+          <button v-show="copied">
+            <Copied />
+          </button>
+          <button v-show="!isCollapse" @click="isCollapse = true">
+            <UnfoldLess />
+          </button>
+          <button v-show="isCollapse" @click="isCollapse = false">
+            <UnfoldMore />
+          </button>
+        </div>
+      </Transition>
+      <Transition v-if="previewUpdateFlag === 'UPDATING'" name="fade">
+        <div class="vue-preview__loading-model">
+          <Loading />
+        </div>
+      </Transition>
+    </div>
+    <MonacoEditor
+      :value="store.state.activeFile.code"
+      :filename="store.state.activeFile.filename"
+      @change="onChange"
+    />
+  </div>
+</template>
+
+<style>
+:root {
+  --vue-preview-radius: 8px;
+  --vue-preview-color-border: hsla(220, 13%, 18%, 0.1);
+  --vue-preview-box-shadow: 2px 4px 8px 4px hsla(0, 0%, 0%, 0.1);
+  --vue-preview-color-icon: hsl(220, 13%, 18%);
+  --vue-preview-color-icon-bg-hover: hsl(220, 95%, 95%);
+  --vue-preview-color-model-bg: hsla(0, 0%, 80%, 0.1);
+}
+</style>
+
+<style lang="scss" scoped>
+.vue-preview {
+  width: 100%;
+  border-radius: var(--vue-preview-radius);
+  overflow: hidden;
+  box-shadow: var(--vue-preview-box-shadow);
+
+  &__container {
+    position: relative;
+    min-height: 128px;
+  }
+
+  :deep(.iframe-container) {
+    box-sizing: border-box;
+    overflow: hidden;
+    height: auto;
+    border: 1px solid var(--vue-preview-color-border);
+    border-radius: var(--vue-preview-radius) var(--vue-preview-radius) 0 0;
+  }
+
+  &__btns {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: flex-end;
+    gap: 4px;
+    padding: 4px;
+    color: var(--vue-preview-color-icon);
+
+    button {
+      cursor: pointer;
+      border: none;
+      padding: 2px;
+      width: 20px;
+      height: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 4px;
+      background-color: transparent;
+
+      &:hover {
+        background-color: var(--vue-preview-color-icon-bg-hover);
+      }
+    }
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.6s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  &__loading-model {
+    position: absolute;
+    inset: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--vue-preview-color-model-bg);
+  }
+
+  :deep(.editor) {
+    box-sizing: border-box;
+    overflow: hidden;
+    height: auto;
+    border-radius: 0 0 var(--vue-preview-radius) var(--vue-preview-radius);
+    max-height: v-bind('maxHeightForCode');
+    transition: max-height 0.3s;
+  }
+}
+
+.vue-preview-slide-down-enter-active,
+.vue-preview-slide-down-leave-active {
+  transition: all 0.3s ease;
+}
+
+.vue-preview-slide-down-enter-from,
+.vue-preview-slide-down-leave-to {
+  transform: translateY(48px);
+  opacity: 0;
+}
+</style>
--- test/main.ts
+++ test/main.ts
@@ -1,55 +1,12 @@
-import { createApp, h, watchEffect } from 'vue'
-import { Repl, ReplStore } from '../src'
-import MonacoEditor from '../src/editor/MonacoEditor.vue'
-// import CodeMirrorEditor from '../src/editor/CodeMirrorEditor.vue'
-import { EditorComponentType } from '../src/editor/types'
+import { createApp, h } from 'vue'
+import VuePreview from './VuePreview.vue'
 ;(window as any).process = { env: {} }
 
 const App = {
   setup() {
-    const query = new URLSearchParams(location.search)
-    const store = ((window as any).store = new ReplStore({
-      serializedState: location.hash.slice(1),
-      showOutput: query.has('so'),
-      outputMode: query.get('om') || 'preview',
-      defaultVueRuntimeURL: import.meta.env.PROD
-        ? undefined
-        : `${location.origin}/src/vue-dev-proxy`,
-      defaultVueServerRendererURL: import.meta.env.PROD
-        ? undefined
-        : `${location.origin}/src/vue-server-renderer-dev-proxy`,
-    }))
-
-    watchEffect(() => history.replaceState({}, '', store.serialize()))
-
-    // setTimeout(() => {
-    // store.setFiles(
-    //   {
-    //     'index.html': '<h1>yo</h1>',
-    //     'main.js': 'document.body.innerHTML = "<h1>hello</h1>"',
-    //     'foo.js': 'document.body.innerHTML = "<h1>hello</h1>"',
-    //     'bar.js': 'document.body.innerHTML = "<h1>hello</h1>"',
-    //     'baz.js': 'document.body.innerHTML = "<h1>hello</h1>"'
-    //   },
-    //   'index.html'
-    // )
-    // }, 1000);
-
-    // store.setVueVersion('3.2.8')
-
     return () =>
-      h(Repl, {
-        store,
-        editor: MonacoEditor as any as EditorComponentType,
-        // layout: 'vertical',
+      h(VuePreview, {
         ssr: true,
-        sfcOptions: {
-          script: {
-            // inlineTemplate: false
-          },
-        },
-        // showCompileOutput: false,
-        // showImportMap: false
       })
   },
 }
